26136238780b3020fc78505664497fc19f414e41
Galbfran <francogalbiati984@gmail.com>
2023-09-20 17:11:34 -0300

    subo get por id

--- src/app/api/tasks/[idTasks]/route.js
+++ src/app/api/tasks/[idTasks]/route.js
@@ -1,18 +1,37 @@
 import {NextResponse} from "next/server"
+import {connectDB} from "../../../../utils/mongoose"
+import Task from '../../../../models/Task'
 
-export function GET(request, {params}){
-    return NextResponse.json({
-        message:`obteniendo tarea ${params.idTasks}`
-    })
+export async function GET(request, {params}){
+    try {
+        await connectDB()
+        const task = await Task.findById(params.idTasks)
+        if(!task) return NextResponse.json({
+            message:"tarea no encontrada"
+        },{
+            status:404
+        })
+        return NextResponse.json(task)
+        
+    } catch (error) {
+        return NextResponse.json({
+            message:error.message
+        },{
+            status:400
+        })
+    }
 }
 
-export function DELETE(request, {params}){
+export async function DELETE(request, {params}){
     return NextResponse.json({
         message:`borrando tarea ${params.idTasks}`
     })
 }
 
-export function PUT(request, {params}){
+export async function PUT(request, {params}){
+    
+
+
     return NextResponse.json({
         message:`modificando tarea ${params.idTasks}`
     })
