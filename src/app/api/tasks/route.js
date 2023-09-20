81f527f30ffe6152d91a8972f02aacf78d5ec27c
Galbfran <francogalbiati984@gmail.com>
2023-09-20 16:54:10 -0300

    subo post funcionando

--- src/app/api/tasks/route.js
+++ src/app/api/tasks/route.js
@@ -8,6 +8,19 @@ export async function GET(){
     return NextResponse.json(tasks)
 }
 
-export function POST(){
-    return NextResponse.json(tasks)
+export async function POST(request){
+    try {
+        const data = await request.json()
+        const task = new Task(data)
+        const savedTask = await task.save()
+        
+        return NextResponse.json(savedTask, {
+            status:200
+        })
+        
+    } catch (error) {
+        return NextResponse.json(error.message, {
+            status:400
+        })
+    }
 }
